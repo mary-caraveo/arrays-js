0f11dba920cb66b03f46ab132c9f9da53f3f1eaf
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-10-04 12:18:18 -0500

    feat: Map

---
+++ server/map.js
@@ -0,0 +1,17 @@
+// Opción 1
+const letters = ['a', 'b', 'c'];
+
+const newArray = [];
+for (let index = 0; index < letters.length; index++) {
+    const element = letters[index];
+    newArray.push(element + '++');
+}
+console.log('original', letters);
+console.log('new', newArray);
+
+//Opción 2
+const letters = ['a', 'b', 'c'];
+const newArray = letters.map(item => item + '++')
+
+console.log('original', letters);
+console.log('new', newArray);
