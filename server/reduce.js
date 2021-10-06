c245fd7f5f35445a8115ee48a458988d2c546a27
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-10-06 12:45:20 -0500

    feat: Reduce

---
+++ server/reduce.js
@@ -0,0 +1,15 @@
+const totals = [1,2,3,4,5];
+
+//Ejemplo for
+let sum = 0;
+for (let index = 0; index < totals.length; index++) {
+    const element = totals[index];
+    sum = sum + element;
+}
+
+console.log(sum);
+
+//Ejemplo reduce
+const rta = totals.reduce((sum, element) => sum + element, 0);
+
+console.log('rta', rta);
