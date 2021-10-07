7af4d117ce84b46c9abc66b2c6de589befdfb1fa
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-10-07 09:20:14 -0500

    feat: Reduce Reloaded

---
+++ server/reduce-reloaded.js
@@ -0,0 +1,54 @@
+//Ejemplo 1
+const items = [1, 3, 2, 3];
+
+const rta = items.reduce((obj, item) => {
+  if (!obj[item]) {
+    obj[item] = 1;
+  } else {
+    obj[item] = obj[item] + 1;
+  }
+  return obj;
+}, {});
+
+console.log(rta);
+
+//Ejemplo 2
+
+const data = [
+    {
+      name: "Nicolas",
+      level: "low",
+    },
+    {
+      name: "Andrea",
+      level: "medium",
+    },
+    {
+      name: "Zulema",
+      level: "hight",
+    },
+    {
+      name: "Santiago",
+      level: "low",
+    },
+    {
+      name: "Valentina",
+      level: "medium",
+    },
+    {
+      name: "Lucia",
+      level: "hight",
+    },
+  ];
+
+  const rta2 = data.map(item => item.level)
+  .reduce((obj, item) => {
+    if (!obj[item]) {
+      obj[item] = 1;
+    } else {
+      obj[item] = obj[item] + 1;
+    }
+    return obj;
+  }, {});
+
+  console.log(rta2);
